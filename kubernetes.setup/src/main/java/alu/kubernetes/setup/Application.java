d3d294f7468997331b11170abcaed004fbf7ad3d
Andreas Lundmark <andreas.erik.lundmark@gmail.com>
2025-02-04 14:22:46 +0100

    add some logging

--- kubernetes.setup/src/main/java/alu/kubernetes/setup/Application.java
+++ kubernetes.setup/src/main/java/alu/kubernetes/setup/Application.java
@@ -1,16 +1,21 @@
 package alu.kubernetes.setup;
 
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-
+		final Logger logger = LoggerFactory.getLogger(Application.class);
 		SpringApplication.run(Application.class, args);
-//		System.out.println("Message app is running!");
-// adding comment
+		logger.info("Application started ------------------>>>>>>");
+
 	}
 
 }
